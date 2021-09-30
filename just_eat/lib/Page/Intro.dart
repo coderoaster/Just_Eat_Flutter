015b52457fe18b681e60c9d62e3bca9d4417452c
coderoaster <66759107+coderoaster@users.noreply.github.com>
2021-09-30 18:01:27 +0900

    미사용 import 제거

--- just_eat/lib/Page/Intro.dart
+++ just_eat/lib/Page/Intro.dart
@@ -1,13 +1,9 @@
-import 'dart:async';
-import 'dart:ffi';
-
 import 'package:flutter/cupertino.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:get/get_navigation/src/extension_navigation.dart';
-import 'package:just_eat/Page/Home.dart';
 
 class IntroPage extends StatefulWidget {
   @override
@@ -39,37 +35,3 @@ class _IntroPageState extends State<IntroPage> {
     return Container();
   }
 }
-
-// class IntroPage extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     CollectionReference users = FirebaseFirestore.instance.collection('data');
-//     print("test : ${users.get()}");
-//    return Container();
-// }
-//  return FutureBuilder<DocumentSnapshot>(
-//       future: users.doc("store").get(),
-//       builder:
-//           (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
-//         if (snapshot.hasError) {
-//           return Text("Something went wrong");
-//         }
-
-//         if (snapshot.hasData && !snapshot.data!.exists) {
-//           return Text("Document does not exist");
-//         }
-
-//         if (snapshot.connectionState == ConnectionState.done) {
-//           Map<String, dynamic> data =
-//               snapshot.data!.data() as Map<String, dynamic>;
-//           return Text("Full Name: ${data}");
-          
-//         }
-//         return Text("loading");
-        
-//       },
-      
-//     );
-    
-//   }
-  
